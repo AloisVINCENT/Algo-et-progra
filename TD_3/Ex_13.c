55f49f7de66d067d95dcd18a8b446edea47dea56
AloisVINCENT <aloisvincent1114@gmail.com>
2022-03-31 19:51:19 +0200

    Ajustements factorielle

--- TD_3/Ex_13.c
+++ TD_3/Ex_13.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main()
-{
-    int n;
-    printf("Entrez un entier : ");
-    scanf("%i", &n);
-    printf("\n%i factorielle = %i\n",n,fact(n));
-}
-
 int fact(int n)
 {
     int a = 1;
+    // Factorielle de 0 = 1
     if (n == 0)
     {
         return 1;
     }
     else
     {
+        // On multiplie tous les nombres de 1 à n
         for (int i = 1; i <= n; i++)
         {
             a = a * i;
         }
         return a;
     }
+}
+
+void main()
+{
+    int n;
+    printf("Entrez un entier : ");
+    scanf("%i", &n);
+    printf("\n%i factorielle = %i\n", n, fact(n));
 }
