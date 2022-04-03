9fc31ebf4ddbd65451dbcc670d69ac276aee189c
AloisVINCENT <aloisvincent1114@gmail.com>
2022-04-03 11:36:13 +0200

    ex 21 fini

--- TD_5/Ex_21.c
+++ TD_5/Ex_21.c
@@ -7,19 +7,71 @@
 int length(char *str)
 {
     int i = 0;
-    while (str++)
+    while (*str++)
     {
         i++;
     }
     return i;
 }
 
+char *mirror(char *str)
+{
+    int l = length(str);
+    char *rts = malloc((l + 1) * sizeof(char));
+    char *test = rts + l;
+    *test-- = '\0';
+    while (*str != '\0')
+    {
+        *test = *str;
+        *str++;
+        *test--;
+    }
+    return rts;
+}
+
+bool palindrome(char *str)
+{
+    for (int i = 0; i < length(str); i++)
+    {
+        if (*str != *mirror(str))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool palindrome2(char *str1, char *str2)
+{
+    if (length(str1) != length(str2))
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = 0; i < length(str1); i++)
+        {
+            if (*str1 != *mirror(str2))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 int main()
 {
     char str[100];
-    char *pstr = NULL;
-    printf("Enter a string \n");
-    scanf("%s", pstr);
-    printf("the length of the string is %i.", length(pstr));
+    char str2[100];
+    char *pstr = str;
+    char *pstr2 = str2;
+    printf("Enter two strings \n");
+    scanf("%s", str);
+    scanf("%s", str2);
+    printf("the length of the string is %i.\n", length(pstr));
+    printf("the mirror of the string is %s.\n", mirror(pstr));
+    printf("%s is a palindrome? %s.\n", pstr, palindrome(pstr) ? "true" : "false");
+    printf("are %s and %s palindromes? %s.\n", pstr, pstr2, palindrome2(pstr, pstr2) ? "true" : "false");
     return 0;
 }
